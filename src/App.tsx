afcf18819ad952cd1b689f0cec624e26b9349d6c
Marcos Martins <marcossilva2016.27@gmail.com>
2023-11-03 10:57:40 -0300

    Creating the overall page structure and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import './App.css'
+// CSS
+import styles from './App.module.css';
+
+// components
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <div></div>
+    <div>
+      <Header />
+      <main className={styles.main}>
+        Conteúdo
+      </main>
+      <Footer />
+    </div>
   )
 }
 
